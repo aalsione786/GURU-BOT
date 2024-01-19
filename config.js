3d53d6df56ed4b97932c4446d4e58821050f64eb
aalsione786 <155665512+aalsione786@users.noreply.github.com>
2024-01-19 19:14:48 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 
 
-global.owner = [
-  ['917605902011', 'GURU', true],
+global.qf jing = [
+  ['923071486486', 'qf jing', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
